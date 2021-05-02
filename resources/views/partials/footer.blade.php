d352581f2319cf3e5ac3c43f5e8dd9e9ee24208d
Ari Lybaert <ari.lybaert@district01.be>
2021-05-02 14:07:51 +0200

    added footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,18 @@
             </div>
+            <footer>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-4 offset-4 m-information">
+                            <h5>by Ari Lybaert</h5>
+                            <h3>Artevelde Hogeschool</h3>
+                            <h5>New Media Development</h5>
+                        </div>
+                        <div class="col-4 m-logo">
+                            <img src="{{ asset('img/logo/ahs.png') }}" alt="">
+                        </div>
+                    </div>
+                </div>
+            </footer>
         @livewireScripts
     </body>
 </html>
